55740cd0e0d107d359235a58587805c3ebb848b0
Michael-Antonio-Dalmacio <2015081255@ust-ics.mygbiz.com>
2018-09-08 07:17:40 +0800

    Initial commit

---
+++ app/src/main/java/com/dalmacio/michael/dalmaciomichaellab1/DalmacioMichaelLab1.java
@@ -0,0 +1,13 @@
+package com.dalmacio.michael.dalmaciomichaellab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DalmacioMichaelLab1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dalmacio_michael_lab1);
+    }
+}
